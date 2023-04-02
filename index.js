6307f7fc383aa8737d8a341a5453bc3fb5b4caa9
Orlando Martinez <orjamar@gmail.com>
2023-04-02 16:04:30 -0500

    Lab resolved

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    function compareNames(driver) {
+      return driver.toLowerCase() === name.toLowerCase();
+    } 
+    return drivers.filter(compareNames);
+}
+
+function fuzzyMatch(drivers, p){
+   function firstLetterMatch(driver){
+    return driver.startsWith(p);
+   }
+   return drivers.filter(firstLetterMatch)
+}
+
+function matchName(drivers, p){
+    function nameMatching(driver){
+        return driver.name === p;
+    }
+    return drivers.filter(nameMatching);
+}
